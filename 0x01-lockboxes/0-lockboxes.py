54ed6a6405963584f533bd64555be1ccf57442f8
surafel27 <surafefekadu399@gmail.com.com>
2023-07-06 14:47:52 -0700

    LockBox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""LockBox implementation"""
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """Lockboxes"""
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes
+    visited[0] = True
+
+    queue = deque([0])
+
+    while queue:
+        current_box = queue.popleft()
+
+        for key in boxes[current_box]:
+            if key >= 0 and key < num_boxes and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
