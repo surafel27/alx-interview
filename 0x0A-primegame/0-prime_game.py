aa5067fbf26a4437092fa63d5301f287f6bac0c7
surafel27 <surafefekadu399@gmail.com.com>
2023-09-08 00:06:28 -0700

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python3
-'''prime game'''
+"""PrimeGame Module.
+"""
 
 
 def isWinner(x, nums):
-    '''define a function to check for prime number'''
-    def is_prime(num):
-        '''check is a number is prime'''
-        if num <= 1:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def play_round(n):
-        '''these is a play ground'''
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        dp = [False] * (n + 1)
-        dp[0] = False
-        dp[1] = False
-
-        for i in range(2, n + 1):
-            if not dp[i]:
-                for prime in primes:
-                    if i % prime == 0:
-                        dp[i] = True
-                        break
-
-        return dp[n]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if play_round(n):
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """prime game."""
+    if not nums or x < 1:
         return None
+    marias_wins = 0
+    bens_wins = 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for _, num in zip(range(x), nums):
+        prime_count = len(list(filter(lambda x: x, primes[0:num])))
+        bens_wins += prime_count % 2 == 0
+        marias_wins += prime_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return "Maria" if marias_wins > bens_wins else "Ben"
